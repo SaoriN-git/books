fda7cb9f5e167d9fd78989d1bbfdba574a99c984
Saori Nishimitsu <seelight.saori@gmail.com>
2023-11-24 12:52:51 +0900

    -

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['password.confirm']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -26,6 +26,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/test', function () {
+  return view('test');
 });
 
 require __DIR__.'/auth.php';

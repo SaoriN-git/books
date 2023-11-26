0c6bcd2c1f567812bd191a19cdc2968079b287de
SaoriNishimitsu <129496603+SaoriNishimitsu@users.noreply.github.com>
2023-11-26 18:35:08 +0900

    after install husky

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -7,19 +7,19 @@ use Illuminate\Http\Request;
 
 class ExampleController extends Controller
 {
-  public function index()
-  {
-    return view('welcome');
-  }
+    public function index()
+    {
+        return view('welcome');
+    }
 
-  public function example()
-  {
-    $examples = Example::all();
-    // $examples = Example::find(1);
-    // $examples = Example::where('id', 1)->get();
-    // $examples = Example::whereIn('id', [1, 2])->get();
-    // $examples = Example::where('id', 1)->where('id', 2)->get();
-    return view('example', ['examples' => $examples]);
-    // return view('example');
-  }
+    public function example()
+    {
+        $examples = Example::all();
+        // $examples = Example::find(1);
+        // $examples = Example::where('id', 1)->get();
+        // $examples = Example::whereIn('id', [1, 2])->get();
+        // $examples = Example::where('id', 1)->where('id', 2)->get();
+        return view('example', ['examples' => $examples]);
+        // return view('example');
+    }
 }
